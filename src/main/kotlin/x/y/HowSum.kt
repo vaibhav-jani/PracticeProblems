7294e481d1009b6bbebb51b449e3558d4a621d08
vaibhav <vaibhavaniljani@gmail.com>
2022-08-20 22:25:43 +0530

    How sum tabulation

--- src/main/kotlin/x/y/HowSum.kt
+++ src/main/kotlin/x/y/HowSum.kt
@@ -7,15 +7,17 @@ fun main() {
     val result = ArrayList<Long>()
     val sum = 7L*/
 
-    val arr1 = arrayOf(14L, 7L)
+    /*val arr1 = arrayOf(14L, 7L)
     val memo = HashMap<Long, ArrayList<Long>?>()
-    val sum = 350L
+    val sum = 350L*/
 
     /*val arr1 = arrayOf(1L, 2L, 5L, 25L)
     val memo = HashMap<Long, ArrayList<Long>?>()
     val sum = 100L*/
 
-    val result = howSum(sum, arr1, memo)
+    val arr1 = arrayOf(4, 7)
+    val sum = 22
+    val result = howSumTable(sum, arr1)
     println("$result")
 
 }
@@ -45,4 +47,33 @@ fun howSum(sum: Long, arr: Array<Long>, memo: HashMap<Long, ArrayList<Long>?>): 
 
     memo[sum] = null
     return null
+}
+
+fun howSumTable(sum: Int, arr: Array<Int>): ArrayList<Int>? {
+
+    val table = Array<ArrayList<Int>?>(sum + 1) {
+        null
+    }
+
+    table[0] = ArrayList()
+    for (index in 0..sum) {
+        if (table[index] != null) {
+            arr.forEach { number ->
+                if (index + number <= sum) {
+                    val newList = ArrayList<Int>()
+                    table[index]?.let {
+                        newList.addAll(it)
+                    }
+                    table[index + number] = newList
+                    table[index + number]?.add(number)
+                }
+            }
+        }
+    }
+
+    for (index in 0..sum) {
+        println(" $index: ${table[index]} ")
+    }
+
+    return table[sum]
 }
