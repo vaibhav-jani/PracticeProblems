6af3fed2de79b4241524bc8b232ab462c3d5ce96
vaibhav <vaibhavaniljani@gmail.com>
2022-08-20 20:57:32 +0530

    Grid traveller tabulation

--- src/main/kotlin/x/y/GridTraveller.kt
+++ src/main/kotlin/x/y/GridTraveller.kt
@@ -3,7 +3,8 @@ package x.y
 fun main() {
 
     val t1 = System.currentTimeMillis()
-    val g1 = gridTraveller(4000, 4000, HashMap())
+    //val g1 = gridTraveller(4000, 4000, HashMap())
+    val g1 = gridTravellerTable(18, 18)
     val t2 = System.currentTimeMillis()
     println("[Took: ${t2 - t1}]")
     println(g1)
@@ -27,4 +28,36 @@ fun gridTraveller(m: Long, n: Long, memo: HashMap<String, Long>): Long {
     val sum = gridTraveller(m - 1, n, memo) + gridTraveller(m, n - 1, memo)
     memo[key] = sum
     return sum
+}
+
+fun gridTravellerTable(m: Int, n: Int): Long {
+
+    val arr = Array(m + 1 ) { i ->
+        Array(n + 1) {
+            0L
+        }
+    }
+
+    arr[1][1] = 1
+    for (i in 0..m) {
+        for (j in 0..n) {
+            val current = arr[i][j]
+            if (j+1 <= n) {
+                arr[i][j+1] += current
+            }
+            if (i+1 <= m) {
+                arr[i+1][j] += current
+            }
+        }
+    }
+
+    /*for (i in 0..m) {
+        for (j in 0..n) {
+            val current = arr[i][j]
+            print(" %12d ".format(current))
+        }
+        println()
+    }*/
+
+    return arr[m][n]
 }
