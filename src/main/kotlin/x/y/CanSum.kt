a3d013a431885f33341d92a0b23417d817524485
vaibhav <vaibhavaniljani@gmail.com>
2022-08-20 21:43:38 +0530

    Can sum tabulation

--- src/main/kotlin/x/y/CanSum.kt
+++ src/main/kotlin/x/y/CanSum.kt
@@ -2,9 +2,11 @@ package x.y
 
 fun main() {
 
-    val arr1 = arrayOf(7L, 14L)
-    val memo = HashMap<Long, Boolean>()
-    println("${canSum(42000L, arr1, memo)}")
+    //val arr1 = arrayOf(7L, 14L)
+    //val memo = HashMap<Long, Boolean>()
+    //println("${canSum(42000L, arr1, memo)}")
+    val arr1 = arrayOf(9, 7, 14)
+    println("${canSumTable(50, arr1)}")
 
 }
 
@@ -34,4 +36,32 @@ fun canSum(sum: Long, arr: Array<Long>, memo: HashMap<Long, Boolean>): Boolean {
     }
 
     return false
+}
+
+fun canSumTable(
+    sum: Int,
+    arr: Array<Int>
+): Boolean {
+
+    val table = Array(sum + 1) {
+        false
+    }
+
+    table[0] = true
+
+    for (index in 0..sum) {
+        if (table[index]) {
+            arr.forEach { number ->
+                if (index + number <= sum) {
+                    table[index + number] = true
+                }
+            }
+        }
+    }
+
+    for (index in 0..sum) {
+        println(" $index: ${table[index]} ")
+    }
+
+    return table[sum]
 }
