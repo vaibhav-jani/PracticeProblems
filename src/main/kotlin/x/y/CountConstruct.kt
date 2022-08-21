c975a474fd58dd7c7a9535c4c7b7f1d8fdd27742
vaibhav <vaibhavaniljani@gmail.com>
2022-08-21 13:56:16 +0530

    Count construct tabulation

--- src/main/kotlin/x/y/CountConstruct.kt
+++ src/main/kotlin/x/y/CountConstruct.kt
@@ -3,8 +3,8 @@ fun main() {
 
     val memo = HashMap<String, Int>()
 
-    /*val str = "abcdef"
-    val arr = arrayOf("ab", "bc", "def", "abc", "abcd", "ef", "abcdef", "c")*/
+    val str = "abcdef"
+    val arr = arrayOf("ab", "bc", "def", "abc", "abcd", "ef", "abcdef", "c")
 
     /*val str = "purple"
     val arr = arrayOf("purp", "p", "ur", "le", "purpl")*/
@@ -12,12 +12,12 @@ fun main() {
     /*val str = "abcdefghijklmopqrstuvwxyzabcdefghijklmopqrstuvwxyzabcdefghijklmopqrstuvwxyz abcdefghijklmopqrstuvwxyzabcdefghijklmopqrstuvwxyzabcdefghijklmopqrstuvwxyzabcdefghijklmopqrstuvwxyzabcdefghijklmopqrstuvwxyzabcdefghijklmopqrstuvwxyzabcdefghijklmopqrstuvwxyzabcdefghijklmopqrstuvwxyzabcdefghijklmopqrstuvwxyz"
     val arr = arrayOf(" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")*/
 
-    val str = "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef"
-    val arr = arrayOf("e", "ee", "eee", "eeee", "eeeee", "eeeeee")
+    /*val str = "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+    val arr = arrayOf("e", "ee", "eee", "eeee", "eeeee", "eeeeee")*/
 
     /*val str = "enterapotentpot"
     val arr = arrayOf("a", "p", "ent", "enter", "ot", "o", "t")*/
-    println(countConstruct(str, arr, memo))
+    println(countConstructTable(str, arr))
 }
 
 fun countConstruct(
@@ -43,9 +43,34 @@ fun countConstruct(
             val countConstruct = countConstruct(remaining, arr, memo)
             count += countConstruct
         }
-        println("--------------------------------")
     }
-    println("===================================")
     memo[str] = count
     return count
+}
+
+fun countConstructTable(
+    target: String,
+    words: Array<String>
+): Int {
+
+    val table = Array(target.length + 1) {
+        0
+    }
+
+    table[0] = 1
+    for (index in 0..target.length) {
+       val current = table[index]
+        if (current > 0) {
+          words.forEach { word ->
+              if (target.substring(index, target.length).startsWith(word)) {
+                  val wordLength = word.length
+                  if ((index + wordLength) <= target.length) {
+                      table[index + wordLength] += current
+                  }
+              }
+          }
+       }
+    }
+
+    return table[target.length]
 }
